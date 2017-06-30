2e1c5a2f6a65878e5fd55c76c0893238fa7556e2
Radoslav Bahchevanov <r.bahchevanov@gmail.com>
2017-06-30 18:49:10 +0300

    Fixed array out of bounds

--- Wavelets/Wavelets.c
+++ Wavelets/Wavelets.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define ARRAYMAX 1024
+#define SQRT2 1.4142135623730950488016887242097
+
 void printArray(float arr[], int n) {
 	for (int i = 0; i < n; i++) {
 		printf("%f ", arr[i]);
@@ -7,22 +10,39 @@ void printArray(float arr[], int n) {
 	printf("\n");
 }
 
-
 void FWT(float* input, int length) {
 	float sum, diff; //sum and difference of elements
 
 	for (int l = length / 2; l >= 1; l /= 2) {
+		
+
+		for (int i = 0; i < l; i++)
+		{
+			sum = input[i * 2] + input[i * 2 + 1];
+			diff = input[i * 2] - input[i * 2 + 1];
+			input[i] = sum / SQRT2;
+			input[l + i] = diff / SQRT2;
+		}
 		printf("l = %d\n", l);
 		printf("array: ");
 		printArray(input, length);
+	}
+}
 
-		for (int i = 0; i < length; i++)
+void FWT_A(float* input, float* output, int length) {
+	float sum, diff; //sum and difference of elements
+	for (int l = length / 2; l >= 1; l /= 2) {
+		for (int i = 0; i < l; i++)
 		{
 			sum = input[i * 2] + input[i * 2 + 1];
 			diff = input[i * 2] - input[i * 2 + 1];
 			input[i] = sum;
-			input[length + i] = diff;
+			input[l + i] = diff;
 		}
+
+		printf("l = %d\n", l);
+		printf("array: ");
+		printArray(input, length);
 	}
 }
 
@@ -30,8 +50,8 @@ int main()
 {
 	float arr[] = { 6, 12, 15, 15, 14, 12, 120, 116 };
 	int n = 8;
-	FWT(arr, n);
 	printArray(arr, n);
+	FWT(arr, n);
 	return 0;
 }
 
